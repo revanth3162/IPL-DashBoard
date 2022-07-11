829dd680dc1298f208c7df1d3abe0285f0ce313f
revanth3162 <revanthbaloju123@gmail.com>
2022-07-11 14:22:02 +0530

    Add Swagger to Application

--- src/main/java/com/springmaven/ipldashbaord/IplDashbaordApplication.java
+++ src/main/java/com/springmaven/ipldashbaord/IplDashbaordApplication.java
@@ -1,17 +1,15 @@
 package com.springmaven.ipldashbaord;
 
-import java.util.Collections;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class IplDashbaordApplication {
 
+
+@SpringBootApplication
+
+public class IplDashbaordApplication {
 	public static void main(String[] args) {
-		SpringApplication app = new SpringApplication(IplDashbaordApplication.class);
-		app.setDefaultProperties(Collections.singletonMap("server.port", "8081"));
-		app.run(args);
 		SpringApplication.run(IplDashbaordApplication.class, args);
 	}
 }
+
